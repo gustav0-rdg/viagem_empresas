2c7c27af60e8fb34bdac881064f08ef3db775bf8
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2025-08-25 14:37:14 -0300

    criando js que vai mostrar o valor da função do html

---
+++ src/js/modules/valore_function.js
@@ -0,0 +1,26 @@
+//function para mostrara valor da function que obteve o valor do html  
+
+export function valoresDaViagem(){
+// valores obtidos do html
+
+constdistanciaKm = document.getElementById('distancia').value;
+const consumoKmPorLitro = document.getElementById('consumo').value;
+const precoCombustivel = document.getElementById('preco').value;
+const velocidadeMedia = document.getElementById('velocidade').value;
+const pedagios = document.getElementById('pedagios').value;
+const alimentacao = document.getElementById('alimentacao').value;
+const hospedagem = document.getElementById('hospedagem').value;
+const outrosCustos = document.getElementById('outros').value;
+
+// retorna os valores obtidos do html
+return{
+    distanciaKm,
+    consumoKmPorLitro,
+    precoCombustivel,
+    velocidadeMedia,
+    pedagios,
+    alimentacao,
+    hospedagem,
+    outrosCustos
+}
+}
